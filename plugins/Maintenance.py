1b8f4a55b6e82a5892b0d283baa698eb6b203864
Praveen Diwakar <praveendiwakar.in@gmail.com>
2025-10-22 17:24:40 +0530

    Update Maintenance.py

--- plugins/Maintenance.py
+++ plugins/Maintenance.py
@@ -11,7 +11,7 @@ async def maintenance_checker(client, message: Message):
     chat_id = message.chat.id
     if MAINTENANCE_MODE and user_id not in BYPASS_IDS and chat_id not in BYPASS_IDS:
         await message.reply(
-            "🚧 **Bot is under Maintenance!**\n\nPlease try again later.\n\nSupport Group: @AV_SUPPORT_GROUP",
+            "🚧 **Bot is under Maintenance!**\n\nPlease try again later.\n\nSupport Group: @CDNChats",
             quote=True
         )
         return
